46f72b3712b7052b298bd196d6eb733eaf53a5e8
kimchi <kimmygao808@gmail.com>
2022-10-14 21:43:33 +1000

    changing project to mobile

--- frontend/src/pages/FriendGoals.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactComponent as Divider } from "../assets/divider.svg";
-import Card from "../components/Card";
-import ContentCard from "../components/ContentCard";
-
-function Homepage() {
-    return <div className="flex flex-col justify-center h-screen place-items-center">friend goal page</div>;
-}
-
-export default Homepage;
